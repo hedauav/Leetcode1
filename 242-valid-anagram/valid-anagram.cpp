3aa0e4a926c919a5b2ac358ff841c8f33c66903c
Anish <hedauav@rknec.edu>
2026-05-24 12:43:16 +0530

    Time: 7 ms (18.87%) | Memory: 9.7 MB (74.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        int n=s.length();
-        int m=t.length();
-        if(n!=m){
+        if(s.length()!= t.length()){
             return false;
         }
-        else{
+        int n=s.length();
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
         for(int i=0;i<n;i++){
             if(s[i]!=t[i]){
                 return false;
             }
-        }
         }
         return true;
     }
